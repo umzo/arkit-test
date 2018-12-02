71df2da762ed2963326cfa82ca8463ba0ee8110b
umzo <umeda2444@gmail.com>
2018-12-02 11:58:07 +0900

    Initial Commit

---
+++ arkit-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  arkit-test
+//
+//  Created by 梅田佳孝 on 2018/12/02.
+//  Copyright © 2018 y-umeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
